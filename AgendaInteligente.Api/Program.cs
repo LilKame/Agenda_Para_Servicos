ac8fe4b28cec44ce92fd0fce26d4cd626f45296d
LucasAnalistDev <lucas.souza@louvreconsulting.com.br>
2026-04-13 14:01:23 -0300

    DataBase

--- AgendaInteligente.Api/Program.cs
+++ AgendaInteligente.Api/Program.cs
@@ -1,41 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using AgendaInteligente.Infrastructure.DataBase;
+using AgendaInteligente.Infrastructure.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
+
+// Swagger
+// 🔹 Swagger (documentação da API)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ AgendaInteligente.Infrastructure/DataBase/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using AgendaInteligente.Domain;
+using AgendaInteligente.Domain.Entities;
+
+namespace AgendaInteligente.Infrastructure.DataBase
+{
+    public class DbInitializer : DbContext
+    {
+        public DbInitializer(DbContextOptions <DbInitializer> options) : base(options){}
+
+        // Tabelas;
+        public DbSet<Company> Company { get; set; }
+
+        // Configurar regras das tabelas;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
