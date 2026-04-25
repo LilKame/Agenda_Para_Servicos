db382e7863e2895cbcc46939cb0c936777ecf956
Efavil <edu.piresfavila@gmail.com>
2026-04-25 18:32:02 -0300

    base do calendario pronta, faltando alguns ajustes

--- AgendaInteligente.Web/src/pages/calendary/calendary.tsx
+++ AgendaInteligente.Web/src/pages/calendary/calendary.tsx
@@ -15,14 +15,12 @@ function Calendary() {
   const [mesExibido, setMesExibido] = useState(new Date());
 
   const inicioMes = startOfMonth(mesExibido);
-  // IMPORTANTE: weekStartsOn: 0 garante que a semana comece no Domingo
   const dataInicialGrade = startOfWeek(inicioMes, { weekStartsOn: 0 });
 
   const diasDaGrade = Array.from({ length: 42 }).map((_, i) => 
     addDays(dataInicialGrade, i)
   );
 
-  // Corrigi os nomes aqui para evitar o erro da foto
   const diasSemana = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
 
   return (
@@ -31,33 +29,31 @@ function Calendary() {
 
       <main className="main-content">
         <div className="calendar-card">
-          
           <header className="calendar-header">
             <button className="nav-btn" onClick={() => setMesExibido(subMonths(mesExibido, 1))}>&lt;</button>
-            <h2 className="month-title">{format(mesExibido, 'MMMM de yyyy', { locale: ptBR })}</h2>
+            <h2 className="month-title">
+              {format(mesExibido, 'MMMM yyyy', { locale: ptBR })}
+            </h2>
             <button className="nav-btn" onClick={() => setMesExibido(addMonths(mesExibido, 1))}>&gt;</button>
           </header>
 
-          {/* Cabeçalho de dias com nomes */}
           <div className="weekdays-grid">
             {diasSemana.map(dia => (
               <div key={dia} className="weekday-cell">{dia}</div>
             ))}
           </div>
 
-          {/* Grade de números */}
           <div className="days-grid">
             {diasDaGrade.map((dia) => (
               <div 
                 key={dia.toISOString()} 
                 className={`day-cell ${!isSameMonth(dia, mesExibido) ? 'neighbor-day' : ''}`}
-                onClick={() => console.log(dia)}
+                onClick={() => console.log("Clicou em:", format(dia, 'dd/MM/yyyy'))}
               >
                 {format(dia, 'd')}
               </div>
             ))}
           </div>
-
         </div>
       </main>
 
