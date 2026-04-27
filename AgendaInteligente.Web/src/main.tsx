3a2f58e6dc59c1871a067b44e41169c7093c63cd
Efavil <edu.piresfavila@gmail.com>
2026-04-27 20:31:59 -0300

    começando a desenvolver o menu de horarios do calendario

--- AgendaInteligente.Web/src/main.tsx
+++ AgendaInteligente.Web/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Calendary from './pages/calendary/Calendary'
+import Calendary from './pages/calendary/calendary'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- AgendaInteligente.Web/src/pages/calendary/calendary.tsx
+++ AgendaInteligente.Web/src/pages/calendary/calendary.tsx
@@ -9,7 +9,7 @@ import {
   subMonths 
 } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import './Calendary.css';
+import './calendary.css';
 
 function Calendary() {
   const [mesExibido, setMesExibido] = useState(new Date());
@@ -30,11 +30,23 @@ function Calendary() {
       <main className="main-content">
         <div className="calendar-card">
           <header className="calendar-header">
-            <button className="nav-btn" onClick={() => setMesExibido(subMonths(mesExibido, 1))}>&lt;</button>
+            <button className="nav-btn" onClick={() => setMesExibido(subMonths(mesExibido, 1))}>
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+              </svg>
+
+            </button>
             <h2 className="month-title">
               {format(mesExibido, 'MMMM yyyy', { locale: ptBR })}
             </h2>
-            <button className="nav-btn" onClick={() => setMesExibido(addMonths(mesExibido, 1))}>&gt;</button>
+            <button className="nav-btn" onClick={() => setMesExibido(addMonths(mesExibido, 1))}>
+              
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
+              </svg>
+              
+              </button>
           </header>
 
           <div className="weekdays-grid">
@@ -53,6 +65,14 @@ function Calendary() {
                 {format(dia, 'd')}
               </div>
             ))}
+          </div>
+        </div>
+        <div className='up-menu-hours'>
+          <div className='hours'>
+            <p>08:00</p>
+            </div>
+          <div className='hours'>
+            <p>09:00</p>
           </div>
         </div>
       </main>
